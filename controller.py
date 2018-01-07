88d0fcbb2dcfb9505b0d275b914c6e3352acd71d
bluephlavio <flavio.grandin@gmail.com>
2018-01-07 21:14:57 +0100

    controller input handling improved.

--- controller.py
+++ controller.py
@@ -1,4 +1,4 @@
-import pygame
+import sys, pygame
 
 pygame.init()
 
@@ -12,25 +12,41 @@ speed = 5
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 
-running = True
+def process_input_1(e):
+    if e.type == pygame.KEYDOWN:
+        if e.key == pygame.K_LEFT:
+            pos[0] -= speed
+        if e.key == pygame.K_RIGHT:
+            pos[0] += speed
+        if e.key == pygame.K_DOWN:
+            pos[1] += speed
+        if e.key == pygame.K_UP:
+            pos[1] -= speed
 
-while running:
+def process_input_2():
+    pressed = pygame.key.get_pressed()
+    if pressed[pygame.K_LEFT]:
+        pos[0] -= speed
+    if pressed[pygame.K_RIGHT]:
+        pos[0] += speed
+    if pressed[pygame.K_DOWN]:
+        pos[1] += speed
+    if pressed[pygame.K_UP]:
+        pos[1] -= speed
+
+
+while True:
 
     clock.tick(50)
-    
+
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
-            running = False
-        if e.type == pygame.KEYDOWN:
-            if e.key == pygame.K_LEFT:
-                pos[0] -= speed
-            if e.key == pygame.K_RIGHT:
-                pos[0] += speed
-            if e.key == pygame.K_DOWN:
-                pos[1] += speed
-            if e.key == pygame.K_UP:
-                pos[1] -= speed
+            sys.exit()
+
+    process_input_2()
 
     screen.fill(bg)
     pygame.draw.circle(screen, color, pos, radius)
     pygame.display.flip()
+
+
