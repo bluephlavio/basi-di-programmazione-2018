ea2ebfef4b19c1ce9cdda05c173a1ab1899c178a
bluephlavio <flavio.grandin@gmail.com>
2018-02-13 16:22:46 +0100

    primality.py algorithm improved. performance measure implemented with time module.

--- primality.py
+++ primality.py
@@ -1,21 +1,33 @@
 # Test per stabilire se un numero intero è primo
 
-# Prende in input il numero da testare
-n = int(input('Inserisci un numero: '))
+import math
+import time
 
-# Inizializza la variabile booleana prime e la lista dei divisori
-prime = True
-dividers = []
+# Prende in input il numero da testare
+n = int(input('Inserisci un numero intero: '))
+
+t1 = time.time()
+
+# Inizializza la variabile booleana 'primo' e la lista 'divisori'
+primo = True
+divisori = []
 
 # Cerca divisori fra 2 e n-1
-for i in range(2, n):
+for i in range(2, int(math.sqrt(n))):
     if n % i == 0:
-        prime = False
-        dividers.append(i)
+        primo = False
+        divisori.append(i)
+        divisori.append(n//i)
 
 # Stampa la primalità del numero
-print('Il numero {} {} primo.'.format(n, 'è' if prime else 'non è'))
+print(f'Il numero {n} {"è" if primo else "non è"} primo.')
 
-# Se il numero non è primo i suoi divisori vengono riportati
-if not prime:
-    print('I divisori sono: ', ', '.join(map(str, dividers)), '.', sep='')
+# Se il numero non è primo, la lista dei suoi divisori viene stampata
+if not primo:
+    divisori.sort() # riordina la lista dei divisori in ordine crescente
+    print(f'Ho trovato {len(divisori)} divisori {str(divisori)}.')
+
+t2 = time.time()
+dt = t2 - t1
+print(f'\nCi ho messo {dt} secondi!')
+input('\nPremi un tasto per uscire...')
