fc0e8985b518be995f17407b0c6f8996ed9947f1
bluephlavio <flavio.grandin@gmail.com>
2018-03-14 21:12:26 +0100

    primality cleaned.

--- primality.py
+++ primality.py
@@ -6,6 +6,7 @@ import time
 # Prende in input il numero da testare
 n = int(input('Inserisci un numero intero: '))
 
+# Legge il tempo iniziale
 t1 = time.time()
 
 # Inizializza la variabile booleana 'primo' e la lista 'divisori'
@@ -20,14 +21,21 @@ for i in range(2, int(math.sqrt(n))):
         divisori.append(n//i)
 
 # Stampa la primalità del numero
-print(f'Il numero {n} {"è" if primo else "non è"} primo.')
+if primo:
+	print(f'Il numero {n} è primo.')
+else:
+	print(f'Il numero {n} non è primo.')
 
 # Se il numero non è primo, la lista dei suoi divisori viene stampata
 if not primo:
     divisori.sort() # riordina la lista dei divisori in ordine crescente
-    print(f'Ho trovato {len(divisori)} divisori {str(divisori)}.')
+    numero_divisori = len(divisori) # Determina il numero di divisori
+    print(f'Ho trovato {numero_divisori} divisori: {divisori}')
 
+# Legge il tempo finale
 t2 = time.time()
+# Calcola quanto ci ha messo
 dt = t2 - t1
+
 print(f'\nCi ho messo {dt} secondi!')
 input('\nPremi un tasto per uscire...')
