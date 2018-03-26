03e501e8f1d6482824f1a88d928154dead551daf
bluephlavio <flavio.grandin@gmail.com>
2018-03-26 14:21:05 +0200

    gas gravity added.

--- gas.py
+++ gas.py
@@ -1,83 +1,118 @@
 import pygame
-import numpy as np
+import random
 
-class Particle:
+# FISICA
+n = 100
+sigma_v = 25
+g = 10
+dt = 0.1
+
+# DIMENSIONI
+dimensioni = (640, 480)
+raggio_particella = 10
+diametro_particella = 2 * raggio_particella
+dimensioni_particella = (diametro_particella,) * 2
+
+# FUNZIONI
+def carica_immagine(nome_file, dimensioni):
+    img = pygame.image.load(f'img\{nome_file}')
+    img = pygame.transform.smoothscale(img, dimensioni)
+    return img
+
+# IMMAGINI
+icona = carica_immagine('icon.jpg', (32, 32))
+sfondo = carica_immagine('bg-light.jpg', dimensioni)
+particella = carica_immagine('star.png', dimensioni_particella)
+
+# CLASSI
+
+class Particella:
 
     def __init__(self, s, v):
-        self.s = np.array(s, dtype=float)
-        self.v = np.array(v, dtype=float)
+        self.s = s
+        self.v = v
 
-    def coordinates(self):
-        return tuple(map(int, self.s))
+    def pos(self):
+        return (self.s[0], self.s[1])
 
-    def update(self, dt, box=None):
-        self.s += self.v * dt
-        if box:
-            x, y = self.coordinates()
-            vx, vy = self.v[0], self.v[1]
-            if (x < 0 and vx < 0) or (x > box.width and vx > 0):
+    def vel(self):
+        return (self.v[0], self.v[1])
+
+    def muovi(self, dt=1, scatola=None):
+        vx, vy = self.vel()
+        self.s[0] += vx * dt
+        self.s[1] += vy * dt
+        urto = False
+        if scatola:
+            x, y = self.pos()
+            larghezza = scatola[0]
+            altezza = scatola[1]
+            if (x < 0 and vx < 0) or (x > larghezza and vx > 0):
                 self.v[0] = -self.v[0]
-            if (y < 0 and vy < 0) or (y > box.height and vy > 0):
+                urto = True
+            if (y < 0 and vy < 0) or (y > altezza and vy > 0):
                 self.v[1] = -self.v[1]
+                urto = True
+        if not urto:
+            self.v[1] += g * dt
 
-    def draw(self, screen):
-        pygame.draw.circle(screen, (255, 255, 255), self.coordinates(), 10)
+    def disegna(self, schermo):
+        x = self.s[0] - raggio_particella
+        y = self.s[1] - raggio_particella
+        schermo.blit(particella, (x, y))
 
 
-class Box:
+class Universo:
 
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.particles = []
-
-    def resize(self, width, height):
-        self.width = width
-        self.height = height
-
-    def size(self):
-        return (self.width, self.height)
-        
-    def create(self, n, sigma):
+    def __init__(self, n, sigma_v, dimensioni=dimensioni):
+        self.dimensioni = dimensioni
+        self.particelle = []
         for i in range(n):
-            s = np.random.uniform((0, 0), (self.width, self.height), 2)
-            v = np.random.normal(0, sigma, 2)
-            particle = Particle(s, v)
-            self.particles.append(particle)
+            x = random.uniform(0, dimensioni[0])
+            y = random.uniform(0, dimensioni[1])
+            vx = random.gauss(0, sigma_v)
+            vy = random.gauss(0, sigma_v)
+            pos = [x, y]
+            vel = [vx, vy]
+            p = Particella(pos, vel)
+            self.particelle.append(p)
 
-    def update(self, dt):
-        for p in self.particles:
-            p.update(dt, box=self)
+    def evolvi(self, dt=1):
+        for p in self.particelle:
+            p.muovi(dt=dt, scatola=self.dimensioni)
 
-    def draw(self, screen):
-        for p in self.particles:
-            p.draw(screen)
+    def disegna(self, schermo):
+        for p in self.particelle:
+            p.disegna(schermo)
 
+# SCHERMO
+pygame.display.set_mode(dimensioni, pygame.RESIZABLE)
+pygame.display.set_caption('Gas')
+pygame.display.set_icon(icona)
+schermo = pygame.display.get_surface()
 
+# TEMPO
+tempo = pygame.time.Clock()
 
-box = Box(640, 480)
-box.create(100, 10)
-screen = pygame.display.set_mode((box.width, box.height), pygame.RESIZABLE)
-clock = pygame.time.Clock()
-dt = .1
+# OGGETTI
+universo = Universo(n, sigma_v, dimensioni=dimensioni)
 
 running = True
-
 while running:
 
-    clock.tick(60)
+    tempo.tick(60)
 
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             running = False
         if e.type == pygame.VIDEORESIZE:
-            width, height = e.size
-            box.resize(width, height)
-            screen = pygame.display.set_mode(box.size(), pygame.RESIZABLE)
+            dimensioni = e.size
+            pygame.display.set_mode(dimensioni, pygame.RESIZABLE)
+            schermo = pygame.display.get_surface()
 
-    screen.fill((0, 0, 0))
-    box.update(dt)
-    box.draw(screen)
+    universo.evolvi(dt=dt)
+
+    schermo.blit(sfondo, (0,0))
+    universo.disegna(schermo)
+
     pygame.display.flip()
-
-        
